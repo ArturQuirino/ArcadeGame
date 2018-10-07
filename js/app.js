b4915db570a07c3721bd5dd6dda5a606d3e6e46e
Artur Quirino <arturquirino6@gmail.com>
2018-10-07 17:04:58 -0300

    Add comments to the code

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
+'use strict';
+
+// Enemies our player must avoid
 var Enemy = function(row, speed) {
     this.x = 0;
     this.y = 60 + row*80;
@@ -5,6 +8,8 @@ var Enemy = function(row, speed) {
     this.sprite = 'images/enemy-bug.png';
 };
 
+// Update the enemy's position
+// Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
   this.x += this.speed*dt;
   if (this.x + 75 > player.x && this.x - 75 < player.x && this.y + 30 > player.y && this.y - 30 < player.y) {
@@ -12,21 +17,27 @@ Enemy.prototype.update = function(dt) {
   }
 };
 
+// Draw the enemy on the screen
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+//Our Player
 var Player = function() {
     this.x = 200;
     this.y = 400;
     this.sprite = 'images/char-boy.png';
-}
+};
 
+//It is a method required by the game
 Player.prototype.update = function(){};
 
+//Draw the player on the screen
 Player.prototype.render = function(){
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
+
+//Handle the input of the player, moving the character up, left, right or down
 Player.prototype.handleInput = function(keyCode){
     switch(keyCode){
         case 'left':
@@ -35,6 +46,7 @@ Player.prototype.handleInput = function(keyCode){
             }
             break;
         case 'up':
+        //if it is moving up, it shoudl check if it is the winning spot.
             if(this.y > 0){
               this.y -= 83
             }
@@ -53,29 +65,40 @@ Player.prototype.handleInput = function(keyCode){
     }
 };
 
+//Check if the player is in the toppest place of the game
 Player.prototype.checkIfWin = function() {
   if (this.y < 0) {
     this.moveToBeginning();
   }
-}
+};
 
+//Move the player to the beginning
 Player.prototype.moveToBeginning = function(){
   this.x = 200;
   this.y = 400;
-}
+};
 
+//List all enemies
 let allEnemies = [];
+
+//Create new enemy every 3 seconds.
 setInterval(createNewEnemy,3000);
+
+//create the first enemy
 createNewEnemy();
 
+//create new enemy with random position and speed
 function createNewEnemy() {
     const speed = Math.random()*70 + 30;
     const row = Math.trunc(Math.random()*3);
     allEnemies.push(new Enemy(row, speed));
-}
+};
+
+//create the player
 let player = new Player();
 
-
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method.
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
